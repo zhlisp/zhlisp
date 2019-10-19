a59a28220e60d6fa17878bc412eeea11def0f26e
陆志广 <zhlisp@aliyun.com>
2019-10-19 19:39:08 +0800

    新加符号2019-10-19
    优化加载函数，添加收集的所有符号

--- ZHLISP中文lispworks编程/ZHLISP中文编程-加载函数.lisp
+++ ZHLISP中文lispworks编程/ZHLISP中文编程-加载函数.lisp
@@ -8,20 +8,33 @@
 (界面 中文LISP)
 
 ;;读代码
-(with-open-file (in (prompt-for-file "请打开ZHLISP中文编程-函数数据.lisp文件" :pathname "D:/GitHub/zhlisp/ZHLISP中文lispworks编程/ZHLISP中文编程-函数数据.lisp"))
-  (when in
-    (loop for line = (read in nil)
-          while line do (when line
-(defmacro abc ()
-`(defmacro ,(car line) (&optional &body body)
-,(format nil "~A" (cdr (cdr line)))
-(SYSTEM::BQ-LIST* (QUOTE ,(car (cdr line))) `(,@body))))(abc))
-)))
+(defun zhlisp (filename &optional (name "请打开'ZHLISP中文编程-函数数据.lisp'文件"))
+	(with-open-file (in (prompt-for-file name :pathname filename))
+		(when in
+			(loop for line = (read in nil)
+				while line do (when line
+					(defmacro zzhlisp ()
+					`(defmacro ,(car (cdr line)) (&optional &body body)
+						,(format nil "~A" (cdr (cdr line)))
+						`(,',(car line) ,@body)))
+					(zzhlisp))))))
 
+(zhlisp "C:/lisp/zhlisp/ZHLISP中文lispworks编程/ZHLISP中文编程-函数数据.lisp" "中文编程")
+
+
+
+
+
+;定义zhlisp
+;(defmacro zhlisp (zh en help)
+;	`(defmacro ,zh (&optional &body body)
+;		,help
+;		`(,',en ,@body)))
+;		
 ;打开
-;(prompt-for-file "lkjljlj" :pathname "~/Downloads/")
+;(prompt-for-file "界面标题" :pathname "~/文名路径名/")
 
 ;保存
-;(with-open-file (out (prompt-for-file "lkjljlj" :pathname "~/Downloads/" :operation :save))
+;(with-open-file (out (prompt-for-file "界面标题" :pathname "~/文名路径名/" :operation :save))
 ;  :direction :output
-;  (format out "abcdefg"))
+;  (format out "保存的文字数据"))
